b65d58d272bebcbfdd6bfb377c8076e26505d80e
Katheci <kratitis.kt@gmail.com>
2026-06-03 23:56:37 -0600

    Fix ID reference for 'Sobre mi' section

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 //Scroll To Sections
 
 function toAbout() {
-  var element = document.getElementById("Sobre mi");
+  var element = document.getElementById("Sobre-mi");
   var headerOffset = 85;
   var elementPosition = element.getBoundingClientRect().top;
   var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
